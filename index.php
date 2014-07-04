a4d39d4d2528aa8ac68d083adae54ee0c22223c6
takaki <takaki@AFN-takaki.local>
2014-07-04 21:15:03 +0900

    third commit

--- index.php
+++ index.php
@@ -4,65 +4,74 @@
 echo date("今日の日付：n月j日\n");
 
 echo date("今日の曜日：");
-if(date(w)==0){
-	echo("日曜");
-}else if(date(w)==1){
-	echo("月曜");
-}else if(date(w)==2){
-	echo("火曜");
-}else if(date(w)==3){
-	echo("水曜");
-}else if(date(w)==4){
-	echo("木曜");
-}else if(date(w)==5){
-	echo("金曜");
-}else if(date(w)==6){
-	echo("土曜");
-}else{
-	echo("曜日エラー");
+if(date(w)==0) {
+echo("日曜");
+}
+if(date(w)==1){
+echo("月曜");
+}
+if(date(w)==2){
+echo("火曜");
+}
+if(date(w)==3){
+echo("水曜");
+}
+if(date(w)==4){
+echo("木曜");
+}
+if(date(w)==5){
+echo("金曜");
+}
+if(date(w)==6){
+echo("土曜");
 }
 
 */
 
 //string date (string $format [, int $timestamp = time()]);
 
-$timestamp = time();
-
 
 //キーワード
 //Y/m/d
 
-$month = date(n,$timestamp);
+//今月のカレンダー作成		
 
-echo"$month ";
+$this_year  = date(Y);
+$this_month = date(m);
+$this_day   = date(d);
 
-$month += 1;
+echo "$this_year".'/'."$this_month".'/'."$this_day";
 
-echo"$month ";
+echo $this_year;
 
-$timestamp += 200000000;
+$last_day = date(t);
 
-$month = date(n,$timestamp);
-
-echo"$month ";
-
-
-
+//今月の1日の曜日を計算(0~6:日~土)
 $start_day = (date(w) - date(j) +8 ) % 7;
-//今月の1日の曜日を計算
+echo "$start_day";
 
-echo"$start_day ";
+/*
+$start_day = date('w', mktime(0, 0, 0, $month, 1, $year));
+echo "$start_day";
+*/
+for ($i = 1; $i <= $last_day; $i++){
+	var_dump($i);
+}
+exit;
 
+//nヶ月後の設定
+$n = 1 ;
 
+//nヶ月後の日付
+//var_dump($target_time);
+$target_time = date('Y/m/d',strtotime("+$n month"));
+var_dump($target_time);
 
-echo $timestamp = date("Y年m月d日",strtotime("+7 month"));
+//nヶ月後の日数
+$last_day = date(t,strtotime("+$n month"));
+var_dump($last_day);
 
-
-echo $lastday = date( t );
-
-echo date( t ,$timestamp);
-
-$n = 7;
-
-echo date( t ,strtotime("+$n month"));
+//nヶ月後の1日の曜日
+$first_day = (date(w,strtotime("+$n month")) - date(j,strtotime("+$n month"))+8)% 7;
+var_dump($first_day);
 
