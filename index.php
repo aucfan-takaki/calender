fc63e1df79a127e5b348c18249fe1583dff8a66b
takaki <takaki@AFN-takaki.local>
2014-07-04 15:07:52 +0900

    second commit

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-echo date("今日：n月j日<br/>");
+/*
+
+echo date("今日の日付：n月j日\n");
+
+echo date("今日の曜日：");
 if(date(w)==0){
 	echo("日曜");
 }else if(date(w)==1){
@@ -17,4 +21,48 @@ if(date(w)==0){
 }else{
 	echo("曜日エラー");
 }
+
+*/
+
+//string date (string $format [, int $timestamp = time()]);
+
+$timestamp = time();
+
+
+//キーワード
+//Y/m/d
+
+$month = date(n,$timestamp);
+
+echo"$month ";
+
+$month += 1;
+
+echo"$month ";
+
+$timestamp += 200000000;
+
+$month = date(n,$timestamp);
+
+echo"$month ";
+
+
+
+$start_day = (date(w) - date(j) +8 ) % 7;
+//今月の1日の曜日を計算
+
+echo"$start_day ";
+
+
+
+echo $timestamp = date("Y年m月d日",strtotime("+7 month"));
+
+
+echo $lastday = date( t );
+
+echo date( t ,$timestamp);
+
+$n = 7;
+
+echo date( t ,strtotime("+$n month"));
 
