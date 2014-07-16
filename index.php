bf6911751e5acdb0d9dc4e1c58000b61971cd2d1
takaki <takaki@AFN-takaki.local>
2014-07-16 15:46:28 +0900

    関数・処理等の整理、カレンダーの横表示

--- index.php
+++ index.php
@@ -1,72 +1,41 @@
 <?php
 
-//年月を取得、nullの場合は今月の値
-$this_year = $_GET["year"];
-$this_month= $_GET["month"];
-if (empty($this_year)) $this_year  = date(Y);
-if (empty($this_month)) $this_month = date(n);
-
-$target_time = mktime(0,0,0,$this_month,1,$this_year);
-
+//$countヶ月後の1日のタイムスタンプ	
 function jumptime($count) {
 	global $target_time;
 	return strtotime($count . ' month', $target_time);
 }
+//$countヶ月後の年の値
 function jumpyear($count) {
 	return date("Y", jumptime($count));
 }
+//$countヶ月後の月の値
 function jumpmonth($count) {
 	return date("n", jumptime($count));
 }
 
-$show_month= $_GET["show"];
-if (empty($show_month)) $show_month  = 3;
-$pmonth = -floor($show_month/2);
-if ($show_month % 2 == 1) {
-	$nmonth = 	floor($show_month/2);
-} else{
-	$nmonth = floor($show_month/2) -1;
-}
-
-$year_months = array();
-for ($i = $pmonth; $i <= $nmonth; $i++) { 
-	$year_months[$i] = array(
-	'time' => jumptime($i),
-	'year' => jumpyear($i),
-	'month' => jumpmonth($i),
-	);
-}
-
-
-
-//今月から($target_time)ヶ月後の日数
-//$last_day = date(t, $target_time);
+//参照月から$targetヶ月後の日数
 function lastday($target) {
 	return date(t, $target);
 }
 
-//今月から($target_time)ヶ月後の1日の曜日を計算(0~6:日~土)
-//$start_day = date(w,$target_time);
+//参照月から$targetヶ月後の1日の曜日を計算(0~6:日~土)
 function startday($target) {
 	return date(w,$target);
 }
 
-$start_days_of_the_week= $_GET["startweek"];
-if (empty($start_days_of_the_week)) $start_days_of_the_week  = 0;
-
 //カレンダーの1マス目の日数
-//$count_day = 1 - $start_day;
 function countday($target) {
 	global $start_days_of_the_week;
 	return 1 - (startday($target) - $start_days_of_the_week + 7) % 7;
 }
 
 //カレンダーの週数
-//$count_week = ceil(($last_day + $start_day)/7);
 function countweek($target) {
 	return ceil((lastday($target) - countday($target) + 1 ) / 7);
 }
 
+//カレンダーの色付け（今日、土日）	
 function addcolor($time, $year, $month, $day){
 	$this_day = strtotime($day-1 . ' day', $time);
 	if (date("Y/m/d",$this_day) == date("Y/m/d")) {
@@ -80,18 +49,7 @@ function addcolor($time, $year, $month, $day){
 	}
 }
 
-$daysname = array(
-	(7 - $start_days_of_the_week) % 7 => '日', 
-	(8 - $start_days_of_the_week) % 7 => '月',
-	(9 - $start_days_of_the_week) % 7 => '火',
-	(10 - $start_days_of_the_week) % 7 => '水',
-	(11 - $start_days_of_the_week) % 7 => '木',
-	(12 - $start_days_of_the_week) % 7 => '金',
-	(13 - $start_days_of_the_week) % 7 => '土',
-	);
-
-echo $daysname[0];
-
+//曜日の色付け（日本語用）
 function startcolor($day){
 	global $start_days_of_the_week;
 	$judge = ($day + $start_days_of_the_week) % 7;
@@ -103,6 +61,50 @@ function startcolor($day){
 	}
 }
 
+
+//年月を取得、nullの場合は今月の値
+$this_year = $_GET["year"];
+$this_month= $_GET["month"];
+if (empty($this_year)) $this_year  = date(Y);
+if (empty($this_month)) $this_month = date(n);
+
+$target_time = mktime(0,0,0,$this_month,1,$this_year);
+
+//表示させるカレンダーの数を取得	、nullの場合は3
+$show_month= $_GET["show"];
+if (empty($show_month)) $show_month  = 3;
+$pmonth = -floor($show_month/2);
+if ($show_month % 2 == 1) {
+	$nmonth = 	floor($show_month/2);
+} else{
+	$nmonth = floor($show_month/2) -1;
+}
+
+//nヶ月分のタイムスタンプ・年・月を配列化
+$year_months = array();
+for ($i = $pmonth; $i <= $nmonth; $i++) { 
+	$year_months[$i] = array(
+	'time' => jumptime($i),
+	'year' => jumpyear($i),
+	'month' => jumpmonth($i),
+	);
+}
+
+//何曜日から表示させるかを取得、nullの場合は0（日曜）
+$start_days_of_the_week= $_GET["startweek"];
+if (empty($start_days_of_the_week)) $start_days_of_the_week  = 0;
+
+//曜日名を配列化
+$daysname = array(
+	(7 - $start_days_of_the_week) % 7 => '日', 
+	(8 - $start_days_of_the_week) % 7 => '月',
+	(9 - $start_days_of_the_week) % 7 => '火',
+	(10 - $start_days_of_the_week) % 7 => '水',
+	(11 - $start_days_of_the_week) % 7 => '木',
+	(12 - $start_days_of_the_week) % 7 => '金',
+	(13 - $start_days_of_the_week) % 7 => '土',
+	);
+
 ?>
 
 <!DOCTYPE html>
@@ -110,7 +112,9 @@ function startcolor($day){
 <head>
 	<style>
 	table {
+		display: inline-table;
 		border-collapse: collapse;
+		margin: 3px;
 	}
 	td {
 		border: solid 1px;
@@ -121,6 +125,9 @@ function startcolor($day){
 	.sunday{background-color: pink;}
 	.saturday{background-color: lightblue;}
 
+	.left{margin:3px; float:left;}
+	.frame{border: solid 1px;padding: 0.5em;}
+	
 	</style>
 	<title></title>
 </head>
@@ -170,23 +177,28 @@ function startcolor($day){
 <form method="GET" action="index.php">
 	<input type="hidden" value=<?php echo jumpyear(-1) ?> name="year">
 	<input type="hidden" value=<?php echo jumpmonth(-1) ?> name="month">
-	<input type="submit" value="＜＜" style="margin:3px; float:left;">
+	<input type="hidden" value=<?php echo $show_month ?> name="show">
+	<input type="hidden" value=<?php echo $start_days_of_the_week ?> name="startweek">
+	<input type="submit" value="＜＜" class="left">
 </form>
 <form method="GET" action="index.php">
 	<input type="hidden" value=<?php echo date("Y") ?> name="year">
 	<input type="hidden" value=<?php echo date("n") ?> name="month">
-	<input type="submit" value="今月" style="margin:3px; float:left;">
+	<input type="hidden" value=<?php echo $show_month ?> name="show">
+	<input type="hidden" value=<?php echo $start_days_of_the_week ?> name="startweek">
+	<input type="submit" value="今月" class="left">
 </form>
 <form method="GET" action="index.php">
 	<input type="hidden" value=<?php echo jumpyear(1) ?> name="year">
 	<input type="hidden" value=<?php echo jumpmonth(1) ?> name="month">
-	<input type="submit" value="＞＞" style="margin:3px">
+	<input type="hidden" value=<?php echo $show_month ?> name="show">
+	<input type="hidden" value=<?php echo $start_days_of_the_week ?> name="startweek">
+	<input type="submit" value="＞＞" >
 </form>
-
 
 <?php foreach ($year_months as $key => $value) :?>
 	<table>
-	<tr><?php echo $value['year'] . '年' . $value['month'] . '月' ?></tr>
+	<tr><td colspan="7" align="center"><?php echo $value['year'] . '年' . $value['month'] . '月' ?></td></tr>
 		<tr>
 			<?php for ($i=0; $i <7 ; $i++) : ?>
 						<td class="<?php
