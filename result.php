5a3ae1d1b55a0648a5197121da08d33a69bac0fc
takaki <takaki@AFN-takaki.local>
2014-08-05 19:41:43 +0900

    数値面修正

--- result.php
+++ result.php
@@ -1,14 +1,5 @@
 <?php
 
-
-//var_dump($_POST);
-
-//新規：値をGET（押した日付）
-//DB接続
-//日付をデフォルト値に
-//入力
-//送信&元の画面に戻る
-
 require_once dirname(__FILE__). '/calendar.php';
 
 //年月を取得、nullの場合は今月の値
@@ -23,11 +14,15 @@ $cal = new Calendar($target_time);
 
 if ($_POST['result'] !== 2) {
 
-$s_time = strtotime($_POST['s_sche_time']);
+$s_time = str_replace(":", ",", strtotime($_POST['s_sche_time']));
 
-$f_time = strtotime($_POST['f_sche_time']);
+var_dump($s_time);
+
+$f_time = str_replace(":", ",", strtotime($_POST['f_sche_time']));
 
 $start_time  = date("Y-m-d H:i:s", mktime(date(H,$s_time), date(i,$s_time), date(s,$s_time), $_POST['s_month'], $_POST['s_day'], $_POST['s_year']));
+
+var_dump($start_time);
 
 $finish_time = date("Y-m-d H:i:s", mktime(date(H,$f_time), date(i,$f_time), date(s,$f_time), $_POST['f_month'], $_POST['f_day'], $_POST['f_year']));
 
--- schedule.php
+++ schedule.php
@@ -1,11 +1,5 @@
 <?php
 
-//新規：値をGET（押した日付）
-//DB接続
-//日付をデフォルト値に
-//入力
-//送信&元の画面に戻る
-
 require_once dirname(__FILE__). '/calendar.php';
 
 //年月を取得、nullの場合は今月の値
@@ -75,12 +69,12 @@ $remark = $cal->select_sche($id, 'remark');
 			<input type="text" name="s_year"  size="3" maxlength="4" value="<?php echo (date(Y,strtotime($s_time['start_at']))) ?>"/>年
 			<input type="text" name="s_month" size="1" maxlength="2" value="<?php echo (date(n,strtotime($s_time['start_at']))) ?>"/>月
 			<input type="text" name="s_day"   size="1" maxlength="2" value="<?php echo (date(j,strtotime($s_time['start_at']))) ?>"/>日
-			<input type="time" name="s_sche_time" step="1" value="00:00:00"/>
+			<input type="time" name="s_sche_time" step="1" value="<?php echo (date("H:i:s",strtotime($s_time['start_at']))) ?>"/>
 			予定終了時間：
 			<input type="text" name="f_year"  size="3" maxlength="4" value="<?php echo (date(Y,strtotime($f_time['finish_at']))) ?>"/>年
 			<input type="text" name="f_month" size="1" maxlength="2" value="<?php echo (date(n,strtotime($f_time['finish_at']))) ?>"/>月
 			<input type="text" name="f_day"   size="1" maxlength="2" value="<?php echo (date(j,strtotime($f_time['finish_at']))) ?>"/>日
-			<input type="time" name="f_sche_time" step="1" value="00:00:00"/>
+			<input type="time" name="f_sche_time" step="1" value="<?php echo (date("H:i:s",strtotime($f_time['finish_at']))) ?>"/>
 			場所：
 			<input type="text" name="place" maxlength="64" value="<?php echo ($place['place']) ?>"/>
 			備考：
@@ -102,16 +96,3 @@ $remark = $cal->select_sche($id, 'remark');
 	<?php endif ?>
 </body>
 </html>
-
-
-
-
-<?php
-
-//idを持っているか判定
-
-//持っていたら「予定削除」ボタン表示
-
-//MySQLのdeleted_atに現在の時刻を入れる
-
-?>
